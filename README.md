a0af2f1021215ebca9cc4f2470647c5361186b69
q1jun <1434834580@qq.com>
2022-11-11 12:30:12 +0800

    优化脚本，添加linux适配

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # 一键登录ecjtu校园网
 
-bySelenium.py 是用selenium登录的版本
-
 byRequests.py 是用requests库实现登录的版本，更轻巧快捷。
+
+## Usage
+
+1. 安装依赖
+```python
+pip3 install -r requirements.txt
+```
+
+2. 自动认证，第一次使用需要输入学号和密码以及运营商并选择当前操作系统
+```python
+python3 byRequsts.py
+```
+
+3. 配置定时执行脚本，从此无需手动认证。（我是在linux软路由跑这个脚本，windows自行百度)
--- byRequsts.py
+++ byRequsts.py
@@ -1,37 +1,39 @@
+import os
+import re
+
 import requests
 from bs4 import BeautifulSoup
-import re
-import os
 
 
 def IdAndPasswd():
     ispList = ["@cmcc", "@unicom", "@telecom"]
+    osList = ["win","linux"]
     try:
-        f = open(".logindata.data", 'r')
-        info = []
-        for a in f.readlines():
-            info.append(a.rstrip('\n'))
-        f.close()
-        if info.__len__() < 3:
+        with open(".logindata.data", 'r') as f:
+            info = [a.rstrip('\n') for a in f]
+        if info.__len__() < 4:
             os.remove(".logindata.data")
-            info.append(str(input("输入学号：")))
-            info.append(str(input("输入密码：")))
+            info.extend((str(input("输入学号：")), str(input("输入密码："))))
             print("1:中国移动  2:中国联通  3:中国电信")
             number = int(input("选择运营商(1-3)："))
             info.append(ispList[number-1])
-            f = open(".logindata.data", 'w')
-            f.write("\n".join(info))
-            f.close()
-    except:
-        info = []
-        info.append(str(input("输入学号：")))
+            print("1:Windows  2:Linux(Macos)")
+            number = int(input("选择操作系统(1-2)："))
+            with open(".logindata.data", 'w') as f:
+                info.append(osList[number-1])
+                f.write("\n".join(info))
+    except Exception:
+        info = [str(input("输入学号："))]
         info.append(str(input("输入密码：")))
         print("1:中国移动  2:中国联通  3:中国电信")
         number = int(input("选择运营商(1-3)："))
         info.append(ispList[number-1])
+        print("1:Windows  2:Linux(Macos)")
+        number = int(input("选择操作系统(1-2)："))
         f = open(".logindata.data", 'w')
-        f.write("\n".join(info))
-        f.close()
+        info.append(osList[number-1])
+        with open(".logindata.data", 'w') as f:
+            f.write("\n".join(info))
     return info
 
 
@@ -39,8 +41,7 @@ def GetIp():
     HOST = "http://172.16.2.100/"
     r = requests.get(HOST)
     bsobj = BeautifulSoup(r.content, 'html5lib')
-    myIp = re.findall(r"ss5=\"(.+?)\"", str(bsobj))[0]
-    return myIp
+    return re.findall(r"ss5=\"(.+?)\"", str(bsobj))[0]
 
 
 def TestLog(requestObj):
@@ -63,20 +64,15 @@ def TestLog(requestObj):
 def showLog(requestObj):
     result = re.findall(r"认证成功页", requestObj.text)
     if(len(result) > 0):
-        for a in range(10):
+        for _ in range(10):
             print("*************** ok ********************")
 
 
 def ACLogin(ip, id, passwd, isp):
     url = "http://172.16.2.100:801/eportal/"
-    headers = {}
-    headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3759.4 Safari/537.36"
-    headers["Referer"] = "http://172.16.2.100/a70.htm?wlanuserip="+ip + \
-        "&wlanacip=null&wlanacname=null&vlanid=0&ip="+ip + \
-        "&ssid=null&areaID=null&mac=00-00-00-00-00-00"
-    headers["Connection"] = "keep-alive"
-    headers["Host"] = "172.16.2.100:801"
-    headers["Origin"] = "http://172.16.2.100"
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3759.4 Safari/537.36", "Referer": f"http://172.16.2.100/a70.htm?wlanuserip={ip}&wlanacip=null&wlanacname=null&vlanid=0&ip={ip}&ssid=null&areaID=null&mac=00-00-00-00-00-00", "Connection": "keep-alive", "Host": "172.16.2.100:801", "Origin": "http://172.16.2.100"}
+
+
     payload = {
         "c": "ACSetting",
         "a": "Login",
@@ -92,39 +88,16 @@ def ACLogin(ip, id, passwd, isp):
         "queryACIP": "0",
         "loginMethod": "1"
     }
-    postData = {
-        "DDDDD": ",0,"+id+isp,
-        "upass": passwd,
-        "R1": "0",
-        "R2": "0",
-        "R3": "0",
-        "R6": "0",
-        "para": "00",
-        "0MKKey": "123456",
-        "buttonClicked": "",
-        "redirect_url": "",
-        "err_flag": "",
-        "username": "",
-        "password": "",
-        "user": "",
-        "cmd": "",
-        "Login": ""
-    }
-    cookiesData = {
-        "ISP_select": isp,
-        "areaID": "null",
-        "ip": ip,
-        "md5_login2": "%2C0%2C"+id+isp+"%7C"+passwd,
-        "program": "test",
-        "ssid": "null",
-        "vlan": "0"
-    }
+    postData = {"DDDDD": f",0,{id}{isp}", "upass": passwd, "R1": "0", "R2": "0", "R3": "0", "R6": "0", "para": "00", "0MKKey": "123456", "buttonClicked": "", "redirect_url": "", "err_flag": "", "username": "", "password": "", "user": "", "cmd": "", "Login": ""}
+
+    cookiesData = {"ISP_select": isp, "areaID": "null", "ip": ip, "md5_login2": f"%2C0%2C{id}{isp}%7C{passwd}", "program": "test", "ssid": "null", "vlan": "0"}
+
     acRequest = requests.post(
         url, params=payload, data=postData, cookies=cookiesData, headers=headers)
     showLog(acRequest)
 
 
-def getIpandMac():
+def getIpandMac_win():
     ipmac = os.popen("ipconfig /all").read()
     ip = re.findall(r"\d+.\d+.\d+.\d+", ipmac)[0]
     mac = re.findall(r"\w+-\w+-\w+-\w+-+\w+-\w+", ipmac)[0]
@@ -133,20 +106,31 @@ def getIpandMac():
         dmac += a
     return (ip, dmac,)
 
+def getIpandMac_linux():
+    ipmac = os.popen("ifconfig -a").read()
+    ip = re.findall(r"\d+.\d+.\d+.\d+", ipmac)[0]
+    mac = re.findall(r"\w+:\w+:\w+:\w+:+\w+:\w+", ipmac)[0]
+    dmac = ""
+    for a in mac.split(":"):
+        dmac += a
+    return (ip, dmac,)
 
-def ACLogout(id, passwd, isp):
-    ip = getIpandMac()[0]
-    mac = getIpandMac()[1]
+
+
+def ACLogout(id, passwd, isp, os):
+    if os == "linux":
+        print(f'os = {os}')
+        ip = getIpandMac_linux()[0]
+        mac = getIpandMac_linux()[1]
+    else:
+        print(f'os = {os}')
+        ip = getIpandMac_win()[0]
+        mac = getIpandMac_win()[1]
 
     url = "http://172.16.2.100:801/eportal/"
-    headers = {}
-    headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3759.4 Safari/537.36"
-    headers["Referer"] = "http://172.16.2.100/a70.htm?wlanuserip="+ip + \
-        "&wlanacip=null&wlanacname=null&vlanid=0&ip="+ip + \
-        "&ssid=null&areaID=null&mac=00-00-00-00-00-00"
-    headers["Connection"] = "keep-alive"
-    headers["Host"] = "172.16.2.100:801"
-    headers["Origin"] = "http://172.16.2.100"
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3759.4 Safari/537.36", "Referer": f"http://172.16.2.100/a70.htm?wlanuserip={ip}&wlanacip=null&wlanacname=null&vlanid=0&ip={ip}&ssid=null&areaID=null&mac=00-00-00-00-00-00", "Connection": "keep-alive", "Host": "172.16.2.100:801", "Origin": "http://172.16.2.100"}
+
+
     payload = {
         "c": "ACSetting",
         "a": "Logout",
@@ -161,21 +145,14 @@ def ACLogout(id, passwd, isp):
         "port": ""
     }
 
-    cookiesData = {
-        "ISP_select": isp,
-        "areaID": "null",
-        "ip": ip,
-        "md5_login2": "%2C0%2C"+id+isp+"%7C"+passwd,
-        "program": "test",
-        "ssid": "null",
-        "vlan": "0"
-    }
+    cookiesData = {"ISP_select": isp, "areaID": "null", "ip": ip, "md5_login2": f"%2C0%2C{id}{isp}%7C{passwd}", "program": "test", "ssid": "null", "vlan": "0"}
+
     acRequest = requests.post(
         url, params=payload, cookies=cookiesData, headers=headers)
     # TestLog(acRequest)
 
 
 info = IdAndPasswd()
-ACLogout(info[0], info[1], info[2])
+ACLogout(info[0], info[1], info[2], info[3])
 ip = GetIp()
 ACLogin(ip, info[0], info[1], info[2])
